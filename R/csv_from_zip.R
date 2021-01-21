fd62cef9f0d4097bb209a3aba8d9c8a9f016f49d
Alex Bhattacharya <alex.bhattacharya@phe.gov.uk>
2021-01-21 17:13:56 +0000

    rename utilties csv_from_zip

--- R/csv_from_zip.R
+++ R/csv_from_zip.R
@@ -1,14 +1,12 @@
-
-
 #' Download a csv from a zip
 #' @param x a zip file from the web
 #'
 #' @return a zip file for ingestion into your chosen readr
 #' @importFrom utils download.file unzip
-#' @export
 #'
 #' @examples head(read.csv(csv_from_zip("https://files.digital.nhs.uk/assets/ods/current/succarc.zip")))
 #'
+#' @export
 
 csv_from_zip <- function(x) {
   loc.url <- x
