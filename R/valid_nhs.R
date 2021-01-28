2101d2cb0d72c35a5b6728874c4d5f1ea317b89b
Alex Bhattacharya <alex.bhattacharya@phe.gov.uk>
2021-01-28 11:15:45 +0000

    added sapply method with nested function to prevent NA error/warnings

--- R/valid_nhs.R
+++ R/valid_nhs.R
@@ -22,60 +22,79 @@
 
 valid_nhs <- function(nhs_number){
 
-  if(!is.na(nhs_number)) {
+  ## create a nested function
+  ## if(is.na(x)) uses the first element of a vector
+  ## sapply of the nested function applies the function to each element
 
-    NHSlength <- nchar(as.character(nhs_number))
+  checksum_algorithm <- function(NHS){
 
-    n1 <- as.numeric(substr(nhs_number, 1, 1))
-    n2 <- as.numeric(substr(nhs_number, 2, 2))
-    n3 <- as.numeric(substr(nhs_number, 3, 3))
-    n4 <- as.numeric(substr(nhs_number, 4, 4))
-    n5 <- as.numeric(substr(nhs_number, 5, 5))
-    n6 <- as.numeric(substr(nhs_number, 6, 6))
-    n7 <- as.numeric(substr(nhs_number, 7, 7))
-    n8 <- as.numeric(substr(nhs_number, 8, 8))
-    n9 <- as.numeric(substr(nhs_number, 9, 9))
-    n10 <- as.numeric(substr(nhs_number, 10, 10))
+    ## immediately fail the missing NHS numbers OR
+    ## if its got the wrong number of digits
 
-    UniformNumberCheck <- ifelse((n1 == n2) &
-                                   (n2 == n3) &
-                                   (n3 == n4) &
-                                   (n4 == n5) &
-                                   (n5 == n6) &
-                                   (n6 == n7) &
-                                   (n7 == n8) &
-                                   (n8 == n9) &
-                                   (n9 == n10),
-                                 1,
-                                 0)
+    if(is.na(NHS)){
 
+      ReturnValue <- 0
 
-    Modulus <- (
-      (n1 * 10) +
-        (n2 * 9) +
-        (n3 * 8) +
-        (n4 * 7) +
-        (n5 * 6) +
-        (n6 * 5) +
-        (n7 * 4) +
-        (n8 * 3) +
-        (n9 * 2)
-    )
+    } else if(nchar(as.character(NHS))!=10) {
 
-    Modulus <- (11 - (Modulus %% 11))
+      ReturnValue <- 0
 
-    ReturnValue <-
-      ifelse(
-        NHSlength == 10 & UniformNumberCheck != 1 &
-          (Modulus == n10 | (Modulus == 11 & n10 == 0)),
-        1, 0)
+    } else {
 
-  } else {
+      ## break up the NHS number into its individual digits
+      n1 <- as.numeric(substr(NHS, 1, 1))
+      n2 <- as.numeric(substr(NHS, 2, 2))
+      n3 <- as.numeric(substr(NHS, 3, 3))
+      n4 <- as.numeric(substr(NHS, 4, 4))
+      n5 <- as.numeric(substr(NHS, 5, 5))
+      n6 <- as.numeric(substr(NHS, 6, 6))
+      n7 <- as.numeric(substr(NHS, 7, 7))
+      n8 <- as.numeric(substr(NHS, 8, 8))
+      n9 <- as.numeric(substr(NHS, 9, 9))
+      n10 <- as.numeric(substr(NHS, 10, 10))
 
-    ReturnValue <- 0
+      ## are all the numbers the same
+      UniformNumberCheck <- ifelse((n1 == n2) &
+                                     (n2 == n3) &
+                                     (n3 == n4) &
+                                     (n4 == n5) &
+                                     (n5 == n6) &
+                                     (n6 == n7) &
+                                     (n7 == n8) &
+                                     (n8 == n9) &
+                                     (n9 == n10),
+                                   1,
+                                   0)
+
+      ## what is the remainder
+      Modulus <- (
+        (n1 * 10) +
+          (n2 * 9) +
+          (n3 * 8) +
+          (n4 * 7) +
+          (n5 * 6) +
+          (n6 * 5) +
+          (n7 * 4) +
+          (n8 * 3) +
+          (n9 * 2)
+      )
+
+      Modulus <- (11 - (Modulus %% 11))
+
+      ## 1 is valid, 0 is not
+      ReturnValue <-
+        ifelse(
+            UniformNumberCheck != 1 &
+            (Modulus == n10 | (Modulus == 11 & n10 == 0)),
+          1, 0)
+
+      }
+
+    return(ReturnValue)
 
   }
 
-  return(ReturnValue)
+  ## to prevent NA errors when looking at a vector of NHS numbers
+  sapply(nhs_number,checksum_algorithm)
 
 }
