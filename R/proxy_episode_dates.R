35e4f848681ed34e0e96cd8eae99a3375fc5355c
Alex Bhattacharya <Alex.Bhattacharya@phe.gov.uk>
2021-02-09 18:53:56 +0000

    working as expected, now with example

--- R/proxy_episode_dates.R
+++ R/proxy_episode_dates.R
@@ -8,37 +8,63 @@
 #' @param x a data frame; will be converted to a data.table
 #' @param patient_group_vars a vector containing any variables to be used for record grouping, minimum is a patient identifier
 #' @param spell_start_date Inpatient provider spell or episode admission date
-#' @param admission_method CDS admission method code
-#' @param admission_source CDS admission source code
 #' @param spell_end_date Inpatient provider spell or episode discharge  date
 #' @param discharge_destination CDS discharge destination code
-#' @param patient_classification CDS patient classification code
+#' @param drop.tmp a logical to drop all tmp values used, default to TRUE
 #'
-#' @return a data.table with cleaned start and end dates, a cip_indx and new cip window dates
+#' @return a data.table with cleaned start and end dates, and an indicator proxy_missing where the value has changed
 #' @export
 #'
 #' @examples
 #'
-#' cip_test <- data.frame(
-#'   id = c('465','465','465','465','8418','8418','8418','8418','8418','8418','8418','8418','26443','26443','26443','33299','33299','33299','33299','33299','33299','33299','33299','33299','33299','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','52635','78915','78915','78915'),
-#'   provider = c('X1T','X1T','X1T','X1T','KHA','KHA','KHA','KHA','KHA','KHA','KHA','KHA','BX2','BX2','BX2','PXH','PXH','PXH','PXH','PXH','PXH','PXH','PXH','PXH','PXH','9HA','9HA','9HA','9HA','9HA','9HA','9HA','9HA','9HA','9HA','9HA','9HA','9HA','9HA','9HA','9HA','YYT','YYT','YYT','YYT','YYT','YYT','YYT','YYT','YYT','YYT','YYT','ABX','ABX','ABX'),
-#'   spell_start = as.Date(c('2020-03-07','2020-03-07','2020-03-25','2020-04-03','2020-01-25','2020-01-26','2020-07-14','2020-08-02','2020-08-12','2020-08-19','2020-08-19','2020-11-19','2019-11-12','2020-04-17','2020-04-23','2020-07-03','2020-01-17','2020-02-07','2020-03-20','2020-04-27','2020-06-21','2020-07-02','2020-10-17','2020-11-27','2021-01-02','2019-12-31','2020-01-02','2020-01-14','2020-01-16','2020-02-07','2020-02-11','2020-02-14','2020-02-18','2020-02-21','2020-02-25','2020-02-28','2020-03-09','2020-03-11','2020-03-12','2020-03-13','2020-03-14','2020-02-04','2020-02-07','2020-02-11','2020-02-14','2020-02-18','2020-02-21','2020-02-25','2020-02-28','2020-03-09','2020-03-11','2020-03-12','2020-04-16','2020-04-24','2020-05-13')),
-#'   spell_end = as.Date(c('2020-03-07','2020-03-25','2020-04-02','2020-04-27','2020-01-25','2020-01-27','2020-07-17','2020-08-07','2020-08-14','2020-08-19','2020-08-22','2020-12-16','2020-04-17','2020-04-23','2020-05-20','2020-07-24','2020-01-28','2020-02-07','2020-03-23','2020-04-29','2020-06-21','2020-07-03','2020-11-27','2021-01-02','2021-01-10','2019-12-31','2020-01-11','2020-01-14','2020-02-04','2020-02-07','2020-02-11','2020-02-14','2020-02-18','2020-02-21','2020-02-25','2020-02-28','2020-03-09','2020-03-11','2020-03-12','2020-03-13','2020-03-30','2020-02-07','2020-02-11','2020-02-14','2020-02-18','2020-02-21','2020-02-25','2020-02-28','2020-03-09','2020-03-11','2020-03-12','2020-03-13','2020-04-24','2020-05-13','2020-06-11')),
-#'   dis_dest = c('51','51','51','54','51','19','19','19','19','51','19','79','51','51','79','65','19','19','19','19','19','29','98','51','79','19','19','19','51','19','19','19','51','51','51','19','19','51','51','19','51','51','51','51','51','51','51','51','51','51','51','51','29','54','19'),
+#' proxy_test <- data.frame(
+#'   id = c(
+#'     rep(3051, 4),
+#'     rep(7835,3),
+#'     rep(9891,3),
+#'     rep(1236,3)
+#'   ),
+#'   provider = c(
+#'     rep("QKJ", 4),
+#'     rep("JSD",3),
+#'     rep("YJG",3),
+#'     rep("LJG",3)
+#'   ),
+#'   spell_start = as.Date(c(
+#'     "2020-07-03", "2020-07-14", "2020-07-23", "2020-08-05",
+#'     "2020-11-01", "2020-11-13", "2020-12-01",
+#'     "2020-03-28", "2020-04-06", "2020-04-09",
+#'     "2020-10-06", "2020-11-05", "2020-12-25"
+#'   )),
+#'   spell_end = as.Date(c(
+#'     "2020-07-11", "2020-07-22", "2020-07-30", "2020-07-30",
+#'     "2020-11-11", NA, "2020-12-03",
+#'     "2020-03-28", NA, "2020-04-09",
+#'     "2020-10-06", "2020-11-05", NA
+#'   )),
+#'   disdest = c(
+#'     19, 19, 51, 19,
+#'     19, 19, 19,
+#'     51, 98, 19,
+#'     19, 19, 98
+#'   )
 #' )
 #'
-#' proxy_episode_dates(x=cip_test,
+#'
+#' proxy_episode_dates(
+#'   x=proxy_test,
 #'   patient_group_vars = c('id','provider'),
 #'   spell_start_date = 'spell_start',
 #'   spell_end_date = 'spell_end',
-#'   discharge_destination = 'dis_dest'
-#' )
+#'   discharge_destination = 'disdest'
+#' )[]
 
 proxy_episode_dates <- function(x,
-                       patient_group_vars,
-                       spell_start_date,
-                       spell_end_date,
-                       discharge_destination) {
+                                patient_group_vars,
+                                spell_start_date,
+                                spell_end_date,
+                                discharge_destination,
+                                drop.tmp = TRUE) {
 
 
   ## convert object if its not already
@@ -77,9 +103,9 @@ proxy_episode_dates <- function(x,
     proxy_missing := data.table::fcase(
       is.na(get(spell_end_date)) & tmp.spell.n == tmp.spell.N, 1,
       is.na(get(spell_end_date)) & tmp.spell.n < tmp.spell.N &
-        get(discharge_destination)=="98", 2,
+        get(discharge_destination) %in% c("51","98"), 2,
       is.na(get(spell_end_date)) & tmp.spell.n < tmp.spell.N &
-        get(discharge_destination)!="98", 3,
+        !get(discharge_destination) %in% c("51","98"), 3,
       !is.na(get(spell_end_date)) &
         get(spell_end_date) < get(spell_start_date), 4,
       default = 0
@@ -109,10 +135,12 @@ proxy_episode_dates <- function(x,
   ]
 
   ## cleanup and remove temp columns
-  tmpcols <- grep("^tmp.",colnames(x),value=TRUE)
-  x[,
-    (tmpcols) := NULL
-  ]
+  if(drop.tmp==TRUE){
+    tmpcols <- grep("^tmp.",colnames(x),value=TRUE)
+    x[,
+      (tmpcols) := NULL
+    ]
+  }
 
   return(x)
 }
