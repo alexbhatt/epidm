55c445e78b1dff59eca0df3962a29aa72043e28e
Alex Bhattacharya <Alex.Bhattacharya@phe.gov.uk>
2021-03-04 01:03:44 +0000

    lookup recode modified for manual mode

--- R/lookup_recode.R
+++ R/lookup_recode.R
@@ -1,16 +1,17 @@
-#' Lookup switch handler
+#' Lookup table switch handler
 #'
 #' A function to call an epidm lookup table and
 #' recode  where we are aware of a new value.
 #'
-#' Currently working with the organism reclassifications and
-#' specimen_type groupings
+#' Built in are  the organism re-classifications and specimen_type groupings
+#' and a manual mode.
 #'
+#' @param col a vector containing the variable to be created/changed
 #' @param type a character value to denote the lookup
-#' @param col a vector containing the organism species name
-#' @param ... two vectors, in the order new,old of another lookup
+#' @param .import a list  in the order list(new,old) containing the
+#' values for another lookup table existing in the environment
 #'
-#' @return a vector containing the updated scientific name for the organism
+#' @return a vector/list object of the recoded field
 #' @export
 #'
 #' @examples
@@ -18,16 +19,27 @@
 #'   sp=respeciate_organism$previous_organism_name,
 #'   ge=gsub("([A-Za-z]+).*", "\\1",respeciate_organism$previous_organism_name)
 #'   )
-#' respec_example$new <- respec_org(respec_example$sp)
+#' respec_example$new <- lookup_recode("species",respec_example$sp)
 #' head(respec_example)
+#'
+#' lookup_recode(
+#'   "ALCALIGENES DENITRIFICANS",
+#'   type = 'manual',
+#'   .import=list(respeciate_organism$organism_species_name,
+#'                respeciate_organism$previous_organism_name)
+#'   )
 
 
-lookup_recode <- function(type,
-                          col,
-                          ...=NULL) {
+lookup_recode <- function(col,
+                          type=c('species','specimen','manual'),
+                          .import = NULL) {
 
 
-  if(type=="species"){
+  if (type == 'manual' & missing(.import)) {
+    stop("supply a two column list for the lookup vector in the format list(new,old)")
+  }
+
+  if(type == "species"){
 
     ## calls upon the lookup table stored in the epidm package
     ## data(respeciate_organism)
@@ -49,11 +61,12 @@ lookup_recode <- function(type,
       )
     )
 
-  } else {
+  } else if (type == 'manual') {
 
-      lk <- as.list(
+    lk <- as.list(
       setNames(
-        ...
+        .import[[1]],
+        .import[[2]]
       )
     )
 
