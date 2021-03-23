c7b26280f23fd4028453751333481208c9db25e9
Alex Bhattacharya <alex.bhattacharya@phe.gov.uk>
2021-03-23 09:27:36 +0000

    initial push of sql_* tools

--- R/sql_readwrite.R
+++ R/sql_readwrite.R
@@ -1,6 +1,21 @@
-sql_read <- function(server,
-                     database,
-                     sql){
+#' Connect to a SQL database
+#'
+#' @description
+#' `r lifecycle::badge('experimental')`
+#'
+#'
+#' An internal function to help setup connections to SQL databases
+#'   acting as a wrapper for the odbc and DBI packages
+#'
+#' @param server a string containing the server connection
+#' @param database a string containing the database name within the data store
+#'
+#' @return
+#' @keywords internal
+#'
+
+sql_connect <- function(server,
+                        database){
 
   # get correct driver for ODBC connection
   if('SQL Server' %in% unique(odbc::odbcListDrivers()$name)) {
@@ -13,39 +28,91 @@ sql_read <- function(server,
     stop("SQL server drivers require installation")
   }
 
+  conString <- paste0('driver={',SQLdriver,'};',
+                      'server=',server,';',
+                      'database=',database,';',
+                      'trusted_connection=true',
+                      'timeout=120')
+
   # connect to the database
-  ODBCConnect <- odbc::dbConnect(
-    odbc(),
-    .connection_string = paste0('driver={',SQLdriver,'};',
-                                'server=',server,';',
-                                'database=',database,';',
-                                'trusted_connection=true',
-                                'timeout=120')
+  odbcConnect <- odbc::dbConnect(
+    odbc::odbc(),
+    .connection_string = conString
   )
 
+  return(odbcConnect)
+}
+
+
+#' Read a table from a SQL database
+#'
+#' @description
+#' `r lifecycle::badge('experimental')`
+#'
+#'
+#' Read a table object to a SQL database. Acts a wrapper for odbc and DBI
+#'   packages.
+#'
+#' @param server a string containing the server connection
+#' @param database a string containing the database name within the data store
+#' @param sql a string containing a SQL query or to a .sql/.txt SQL query
+#'
+#' @return
+#' @export
+#'
+sql_read <- function(server,
+                     database,
+                     sql){
+
+  odbcConnect <- sql_connect(server = server, database = database)
+
   # test the connection is valid
-  if(DBI::dbIsValid(ODBCConnect)){
-    tableResult <- dbGetQuery(ODBCConnect,
-                              epidm::clean_sql(sql))
+  if(DBI::dbIsValid(odbcConnect)){
+
+    sqlQuery <- epidm::sql_clean(sql)
+
+    # query te database
+    tableResult <- DBI::dbGetQuery(conn = odbcConnect,
+                                   statement = sqlQuery)
   }
 
   return(tableResult)
 
   # close the connection
-  DBI::dbDisconnect(ODBCConnect)
+  DBI::dbDisconnect(odbcConnect)
 
 }
 
 
+
+#' Write a table to a SQL database
+#'
+#' @description
+#' `r lifecycle::badge('experimental')`
+#'
+#'
+#' Write a table object to a SQL database. Acts a wrapper for odbc and DBI
+#'   packages with additional checks to ensure upload completes.
+#'
+#' @param x a data.frame/data.table/tibble object
+#' @param server a string containing the server connection
+#' @param database a string containing the database name within the data store
+#' @param tablename a string containing the chosen SQL database table name
+#'
+#' @return
+#' @export
+#'
 sql_write <- function(x,
                       server,
                       database,
                       tablename){
 
+
+
   check_SQL <- paste0('SELECT COUNT(*) FROM ',DLdatabase,'.dbo.HospitalOnset_COVID')
 
-  if(exists('hcai')){
-    if(nrow(hcai)>0){
+  if(exists('x')){
+    if(nrow(x)>0){
       if(DBI::dbIsValid(odbc_datalake)){
         print('connection established')
       } else{
@@ -63,7 +130,7 @@ sql_write <- function(x,
 
         dbWriteTable(conn = odbc_datalake,
                      name = DBI::Id(schema  = 'dbo',
-                                    table   = 'HospitalOnset_COVID'),
+                                    table   = tablename),
                      value = hcai,
                      encoding = 'latin1',
                      row.names = FALSE,
