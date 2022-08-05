8532db19155c20d14989b1256549ee915caf4aea
Alex Bhattacharya <alex.bhattacharya@ukhsa.gov.uk>
2022-08-05 21:52:32 +0100

    added discharge with GP follow up to ECDS lookup

--- data-raw/inpatient_lookups.R
+++ data-raw/inpatient_lookups.R
@@ -4,71 +4,57 @@
 
 group_inpatient_discharge_destination <- rbind(
   data.frame(
-    code = c("19","29"),
+    code = c("19", "29"),
     discharge_destination = "Patient returned home"
   ),
   data.frame(
-    code = c("50","51","52","53","30","48","49"),
+    code = c("50", "51", "52", "53", "30", "48", "49"),
     discharge_destination = "Patient discharged to another NHS Provider"
   ),
   data.frame(
-    code = c("54","85"),
+    code = c("54", "85"),
     discharge_destination = "Patient discharged to Care Home"
   ),
   data.frame(
-    code = c("65","66"),
+    code = c("65", "66"),
     discharge_destination = "Patient discharged to Local Authority Care"
   ),
   data.frame(
-    code = c("84","87","88"),
+    code = c("84", "87", "88"),
     discharge_destination = "Patient discharged to non-NHS Hospital"
   ),
   data.frame(
-    code = c("37","38"),
+    code = c("37", "38"),
     discharge_destination = "Patient in justice system"
   ),
-  data.frame(
-    code = c("79"),
-    discharge_destination = "Patient died"
-  ),
-  data.frame(
-    code = c("98"),
-    discharge_destination = "Patient still in Hospital"
-  ),
-  data.frame(
-    code = c("99",NA),
-    discharge_destination = "Not known"
-  )
-  )
+  data.frame(code = c("79"),
+             discharge_destination = "Patient died"),
+  data.frame(code = c("98"),
+             discharge_destination = "Patient still in Hospital"),
+  data.frame(code = c("99", NA),
+             discharge_destination = "Not known")
+)
 
 
 
 
 group_inpatient_admission_method <- rbind(
   data.frame(
-    code = c("11","12","13"),
+    code = c("11", "12", "13"),
     admission_method = "Elective admission"
   ),
   data.frame(
-    code = c("21","22","23","24","28","2A","2B","2C","2D"),
+    code = c("21", "22", "23", "24", "28", "2A", "2B", "2C", "2D"),
     admission_method = "Emergency admission"
   ),
-  data.frame(
-    code = c("31","32"),
-    admission_method = "Maternity admission"
-  ),
-  data.frame(
-    code = c("82","83"),
-    admission_method = "Baby born in hospital"
-  ),
-  data.frame(
-    code = c("81"),
-    admission_method = "Patient transfer into hospital"
-  ),
-  data.frame(
-    code = c(NA),
-    admission_method = "Not known"
-  )
+  data.frame(code = c("31", "32"),
+             admission_method = "Maternity admission"),
+  data.frame(code = c("82", "83"),
+             admission_method = "Baby born in hospital"),
+  data.frame(code = c("81"),
+             admission_method = "Patient transfer into hospital"),
+  data.frame(code = c(NA),
+             admission_method = "Not known")
 )
 
 ## These are for Emergency Care Data Set (ECDS) A&E attendance discharge codes
@@ -77,37 +63,44 @@ group_inpatient_admission_method <- rbind(
 group_ecds_discharge_destination <-
   rbind(
     data.frame(
-      code = c("306689006",
-                "306691003",
-                "306694006",
-                "306705005",
-                "50861005",
-               NA),
+      code = c(
+        "306689006",
+        "306691003",
+        "306694006",
+        "306705005",
+        "50861005",
+        "989501000000106", # discharged for follow up by GP
+        NA
+      ),
       destination_code = "Discharged"
     ),
     data.frame(
       code = c("1066331000000109",
-                "1066341000000100",
-                "1066351000000102"),
-      destination_code = "Ambulatory/Short stay"),
+               "1066341000000100",
+               "1066351000000102"),
+      destination_code = "Ambulatory/Short stay"
+    ),
     data.frame(
-      code = c("306706006",
-                "1066361000000104",
-                "1066371000000106",
-                "1066381000000108",
-                "1066391000000105",
-                "1066401000000108"),
-      destination_code = "Admitted"),
-    data.frame(
-      code = c("19712007"),
-      destination_code = "Transfer"),
-    data.frame(
-      code = c("305398007"),
-      destination_code = "Died")
+      code = c(
+        "306706006",
+        "1066361000000104",
+        "1066371000000106",
+        "1066381000000108",
+        "1066391000000105",
+        "1066401000000108"
+      ),
+      destination_code = "Admitted"
+    ),
+    data.frame(code = c("19712007"),
+               destination_code = "Transfer"),
+    data.frame(code = c("305398007"),
+               destination_code = "Died")
   )
 
-usethis::use_data(group_inpatient_admission_method,
-                  group_inpatient_discharge_destination,
-                  group_ecds_discharge_destination,
-                  overwrite = TRUE,
-                  internal = TRUE)
+usethis::use_data(
+  group_inpatient_admission_method,
+  group_inpatient_discharge_destination,
+  group_ecds_discharge_destination,
+  overwrite = TRUE,
+  internal = TRUE
+)
