a5239943ce746dd809cbfa098a50bbc1bf3f0785
Owen.Pullen <Owen.Pullen@ukhsa.gov.uk>
2023-08-09 18:16:44 +0100

    SQL upload unit tests

---
+++ tests/testthat/test-sql_upload_from_csv.R
@@ -0,0 +1,16 @@
+testthat::test_that("sql-upload test csv", {
+    test_df <- readr::read_csv(readr::readr_example("mtcars.csv"))
+    con <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+    DBI::dbWriteTable(con, name = DBI::SQL("example_mtcars"), value = test_df,
+                      overwrite = TRUE)
+    expected <- DBI::dbReadTable(con, "example_mtcars")
+    sql_upload_from_csv(input_filename = readr::readr_example("mtcars.csv"),
+                        con = con,
+                        schema = NULL,
+                        table_name = "test_mtcars",
+                        chunk_size = 5
+    )
+    test <- DBI::dbReadTable(con, "test_mtcars")
+    DBI::dbDisconnect(con)
+    testthat::expect_equal(test, expected)
+})
---
+++ tests/testthat/test-sql_upload_from_txt.R
@@ -0,0 +1,17 @@
+testthat::test_that("sql-upload test txt", {
+    test_df <- readr::read_delim(readr::readr_example("whitespace-sample.txt"))
+    con <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+    DBI::dbWriteTable(con, name = DBI::SQL("example_sample"), value = test_df,
+                      overwrite = TRUE)
+    expected <- DBI::dbReadTable(con, "example_sample")
+    sql_upload_from_txt(input_filename = readr::readr_example("whitespace-sample.txt"),
+                        con = con,
+                        schema = NULL,
+                        table_name = "test_sample",
+                        chunk_size = 1,
+                        delim = " "
+    )
+    test <- DBI::dbReadTable(con, "test_sample")
+    DBI::dbDisconnect(con)
+    testthat::expect_equal(test, expected)
+})
