84840c5019fffedffbcbae9ad782978ca9433547
Owen.Pullen <Owen.Pullen@ukhsa.gov.uk>
2023-08-10 18:06:23 +0100

    validate nhs number tests and rd

---
+++ tests/testthat/test-validate_nhs_number.R
@@ -0,0 +1,41 @@
+testthat::test_that("Test validate NHS number", {
+  t <- rep(c("test", NA), 10)
+  nhs_numbers <- nhsnumbergenerator::generate_nhs_number(n = 100)
+  expect_equal(validate_nhs_number(c(nhs_numbers,t), TRUE), c(nhs_numbers, rep(NA_character_,20)))
+  expect_equal(validate_nhs_number(c(nhs_numbers,t), FALSE), c(rep(1, 100), rep(0, 20)))
+  nhs_numbers <- data.frame(test = nhs_numbers)
+  # test output
+  expect_equal(
+    dplyr::mutate(nhs_numbers, test = validate_nhs_number(test, TRUE)), nhs_numbers)
+  # Speed tests - if don't pass due to low machine RAM (acceptable) otherwise re-engineer
+  expect_lt(
+    as.numeric(dplyr::select(rbenchmark::benchmark(dplyr::mutate(nhs_numbers,test = validate_nhs_number(test)),
+                                                   replications = 25), elapsed)),
+    as.numeric(0.1)
+  )
+
+  expect_lt(
+    as.numeric(dplyr::select(rbenchmark::benchmark(
+      dplyr::mutate(nhs_numbers,test = validate_nhs_number(test, TRUE)), replications = 25), elapsed)),
+    as.numeric(0.1)
+    )
+  # invalid values
+  expect_equal(validate_nhs_number(NA_character_, TRUE), NA_character_)
+  expect_equal(validate_nhs_number(NA_character_, FALSE), 0)
+  test_2 <- data.frame(test = c(NA, "test", ",", "", " ", "\t", NA_character_,
+                                NA_complex_, NA_integer_, NA_real_, 12345, 64578,
+                                5555555555, 9999999999))
+  test_2 <- dplyr::union_all(nhs_numbers,test_2)
+  test_3 <- dplyr::mutate(dplyr::union_all(nhs_numbers, data.frame(test = c(NA, NA, NA, NA, NA, NA, NA,
+                                NA, NA, NA, NA, NA,NA, NA))),
+                                test = as.character(test)
+  )
+   expect_equal(dplyr::mutate(test_2, test = validate_nhs_number(test, TRUE)), test_3)
+   expect_equal(dplyr::mutate(test_2, test = validate_nhs_number(test, FALSE)),
+                          dplyr::mutate(test_3, test = dplyr::case_when(is.na(test) ~ 0, TRUE ~ 1))
+   )
+   # valid_nhs
+   expect_equal(dplyr::mutate(test_3, test2 = validate_nhs_number(test, FALSE)),
+                dplyr::mutate(test_3, test2 = as.integer(epidm::valid_nhs(test)))
+   )
+})
