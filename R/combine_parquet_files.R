1652751a5f9939529e7f3e71880d69ca6be58d1f
owen.pullen <owen.pullen@ukhsa.gov.uk>
2023-08-14 16:03:24 +0100

    add error handling

--- R/combine_parquet_files.R
+++ R/combine_parquet_files.R
@@ -28,6 +28,7 @@
 #'
 combine_parquet_files <- function(path, pattern, backup_name, remove_old = FALSE) {
   sources <- list.files(path = path, pattern = pattern, full.names = TRUE)
+  if (length(sources) <= 1) {stop("Cannot combine a single or 0 parquet files, check path and directory")}
   sources <- sources[order(file.info(sources)$ctime, grep("[0-9]", sources))]
   arrow::write_parquet(arrow::open_dataset(sources, format = "parquet"),
                        sink = paste0(path, backup_name, ".parquet")
